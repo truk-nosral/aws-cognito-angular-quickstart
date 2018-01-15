90c5b4fb1b505781962eba1bf5732fa736c30efb
truk-nosral <kurtl@slalom.com>
2018-01-15 14:32:11 -0800

    env changes after createResources is run. add '.' prefix to base href to get the app working from S3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,23 +1,22 @@
 export const environment = {
     production: false,
 
-    region: 'us-east-1',
+    region: 'us-east-2',
 
-    identityPoolId: 'us-east-1:fbe0340f-9ffc-4449-a935-bb6a6661fd53',
-    userPoolId: 'us-east-1_PGSbCVZ7S',
-    clientId: 'hh5ibv67so0qukt55c5ulaltk',
+    identityPoolId: '',
+    userPoolId: 'us-east-2_POWHQ7fJz',
+    clientId: '7hfpf2l7saq9m1sp9mtvre21sj',
 
     rekognitionBucket: 'rekognition-pics',
     albumName: "usercontent",
-    bucketRegion: 'us-east-1',
+    bucketRegion: 'us-east-2',
 
-    ddbTableName: 'LoginTrail',
+    ddbTableName: 'LoginTrailkurt-2',
 
     cognito_idp_endpoint: '',
     cognito_identity_endpoint: '',
     sts_endpoint: '',
     dynamodb_endpoint: '',
     s3_endpoint: ''
-
 };
 
